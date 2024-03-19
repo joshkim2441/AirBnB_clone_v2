f41097dcad111a70da826e2a2f793ecd69d2ea8d
joshkim2441 <joshkim2441@gmail.com>
2024-03-19 21:55:57 +0300

    Console

--- console.py
+++ console.py
@@ -2,10 +2,8 @@
 """ Console Module """
 import cmd
 import sys
-from shlex import split
-from models import storage
-from datetime import datetime
-from models.base_model import BaseModel
+from models.__init__ import storage
+from models.base_model import BaseModel, Base
 from models.user import User
 from models.place import Place
 from models.state import State
