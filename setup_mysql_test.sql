1842e7d9ffeea1ca03eb6522d822518c3a596bea
joshkim2441 <joshkim2441@gmail.com>
2024-03-19 21:29:17 +0300

    SQL user permissions

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -8,9 +8,8 @@ SET PASSWORD
     FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES
    ON `hbnb_test_db`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
+   TO 'hbnb_test'@'localhost';
 GRANT SELECT
    ON `performance_schema`.*
-   TO 'hbnb_test'@'localhost'
+   TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
