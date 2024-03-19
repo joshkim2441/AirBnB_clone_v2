269176d65a4c1ca939a61472665fec03e3091d42
joshkim2441 <joshkim2441@gmail.com>
2024-03-19 21:44:44 +0300

    SQL user permissions

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER
     IF NOT EXISTS 'hbnb_test'@'localhost';
 SET PASSWORD
-    FOR 'hbnb_dev'@'localhost' = 'hbnb_test_pwd';
+    FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES
    ON `hbnb_test_db`.*
    TO 'hbnb_test'@'localhost';
