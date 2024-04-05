f1d3ca5e70816dd031448c71ea25f7d6205a1cbb
joshkim2441 <joshkim2441@gmail.com>
2024-04-05 04:53:10 +0300

    AirBnB clone v2

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -66,25 +66,14 @@ def deploy():
 
 
 def do_clean(number=0):
-    """Deletes out-of-date archives of the static files.
-    Args:
-        number (Any): The number of archives to keep.
-    """
-    archives = os.listdir('versions/')
-    archives.sort(reverse=True)
-    start = int(number)
-    if not start:
-        start += 1
-    if start < len(archives):
-        archives = archives[start:]
+    """ Deletes out_of_date archives """
+    number = int(number)
+    if number < 2:
+        number = 1
     else:
-        archives = []
-    for archive in archives:
-        os.unlink('versions/{}'.format(archive))
-    cmd_parts = [
-        "rm -rf $(",
-        "find /data/web_static/releases/ -maxdepth 1 -type d -iregex",
-        " '/data/web_static/releases/web_static_.*'",
-        " | sort -r | tr '\\n' ' ' | cut -d ' ' -f{}-)".format(start + 1)
-    ]
-    run(''.join(cmd_parts))
+        number += 1
+
+    paths = ["versions/*", "/data/web_static/releases/*"]
+    for path in paths:
+        cmd = "ls -dt {} | tail -n {} | xargs rm -rf".format(path, number)
+        local(cmd) if path == "versions/*" else run(cmd)
