c379ec861b735f2083202e71ab9db9c3441df8d4
joshkim2441 <joshkim2441@gmail.com>
2024-04-07 14:50:37 +0300

    AirBnB clone v2

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -13,45 +13,42 @@ env.hosts = ['34.232.53.167', '54.89.195.92']
 created_path = None
 
 
+@runs_once
 def do_pack():
+    """ Generates a .tgz archive from contents
+    of the web_static folder
     """
-        generates a .tgz archine from contents of web_static
-    """
-    time = datetime().strftime('%Y%m%d%H%M%S')
-    file_name = "versions/web_static_{}.tgz".format(time)
-    try:
-        local("mkdir -p ./versions")
-        local("tar --create --verbose -z --file={} ./web_static"
-              .format(file_name))
-        return file_name
-    except:
+    tm = datetime.now()
+    tm_ft = tm.strftime("%Y%m%d%H%M%S")
+    if not os.path.isdir("versions"):
+        local("mkdir versions")
+    file_path = "versions/web_static_{}.tgz".format(tm_ft)
+    result = local("tar -cvzf {} web_static".format(file_path))
+    if result.failed:
         return None
+    archize_size = os.stat(file_path).st_size
+    print("web_static packed: {} -> {} Bytes".format(file_path, archize_size))
+    return file_path
 
 
 def do_deploy(archive_path):
-    """
-        using fabric to distribute archive
-    """
-    if os.path.isfile(archive_path) is False:
+    """distributes an archive to the web servers"""
+    if os.path.exists(archive_path) is False:
         return False
     try:
-        archive = archive_path.split("/")[-1]
-        path = "/data/web_static/releases"
-        put("{}".format(archive_path), "/tmp/{}".format(archive))
-        folder = archive.split(".")
-        run("mkdir -p {}/{}/".format(path, folder[0]))
-        new_archive = '.'.join(folder)
-        run("tar -xzf /tmp/{} -C {}/{}/"
-            .format(new_archive, path, folder[0]))
-        run("rm /tmp/{}".format(archive))
-        run("mv {}/{}/web_static/* {}/{}/"
-            .format(path, folder[0], path, folder[0]))
-        run("rm -rf {}/{}/web_static".format(path, folder[0]))
-        run("rm -rf /data/web_static/current")
-        run("ln -sf {}/{} /data/web_static/current"
-            .format(path, folder[0]))
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except:
+    except Exception:
         return False
 
 
