a308a01eecf07fec7a8b6336cf2e6ea3cd1b1bd4
joshkim2441 <joshkim2441@gmail.com>
2024-04-19 17:04:31 +0300

    Webflask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,20 +5,22 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-    def hello_hbnb():
-        """ returns 'Hello HBNB' """
-        return "Hello HBNB"
+def hello_hbnb():
+    """ returns 'Hello HBNB' """
+    return "Hello HBNB"
 
 
 @app.route("/hbnb", strict_slashes=False)
-    def hbnb():
-        """ return 'HBNB' """
-        return "HBNB"
+def hbnb():
+    """ return 'HBNB' """
+    return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
-    def text():
-        """returns text given """
-        return "C {}".format(text.replace("_", " "))
+def text():
+    """returns text given """
+    return "C {}".format(text.replace("_", " "))
+
 
 if __name__ == "__main__":
     app.run()
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,32 +1,35 @@
 #!/usr/bin/python3
 """ Flask framework """
 from flask import Flask
-
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
-    def hello_hbnb():
-        """ returns 'Hello Hbnb' """
-        return "Hello HBNB!"
+def hello_hbnb():
+    """ returns 'Hello Hbnb' """
+    return "Hello HBNB!"
+
 
 @app.route("/HBNB", strict_slashes=False)
-    def HBNB():
-        """ returns 'HBNB' """
-        return "NBNB"
+def HBNB():
+    """ returns 'HBNB' """
+    return "NBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
-    def text(text):
-        """ returns text given """   
-        return "C {}".format(text.replace("_", " "))
+def text(text):
+    """ returns text given """
+    return "C {}".format(text.replace("_", " "))
+
 
 @app.route("/python", defaults={'text': 'is cool'})
 @app.route("/python/text", strict_slashes=False)
 def display(text):
-        """ display 'python' followed by the
-        value of the text
-        """
-        return "Python 
-        {}".format(text.replace("_", " "))
+    """ display 'python' followed by the
+    value of the text
+    """
+    return "Python {}".format(text.replace("_", " "))
 
-if _name__ == "__main__":
+
+if __name__ == "__main__":
     app.run()
