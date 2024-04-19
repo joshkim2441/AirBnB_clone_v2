eb92fe588433f37e6347597287a62d732265ff97
joshkim2441 <joshkim2441@gmail.com>
2024-04-19 09:53:14 +0300

    web flask

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# Starts a Flask web applicaton
+
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Displays 'Hello hbnb!' """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Displays 'HBNB' """
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
