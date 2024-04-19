b0e29f219b56290933cef622d4c09ac3e8db7c50
Joshua Kimuli <joshkim2441@gmail.com>
2024-04-19 14:40:41 +0300

    web flask

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Flask framework """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+    def hello_hbnb():
+        """ returns 'Hello HBNB' """
+        return "Hello HBNB"
+
+
+@app.route("/hbnb", strict_slashes=False)
+    def hbnb():
+        """ return 'HBNB' """
+        return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+    def text():
+        """returns text given """
+        return "C {}".format(text.replace("_", " "))
+
+if __name__ == "__main__":
+    app.run()
