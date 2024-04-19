f9cd4078cce0c51a95a09d49f4990d2031222104
joshkim2441 <joshkim2441@gmail.com>
2024-04-19 10:51:50 +0300

    web flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,11 +6,11 @@ import flask
 app = flask.Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
     """ Displays 'Hello hbnb!' """
     return "Hello HBNB!"
 
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
