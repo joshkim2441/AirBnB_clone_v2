ee8466838c2a9643fce3b990c22ec635fc7f581d
joshkim2441 <joshkim2441@gmail.com>
2024-04-19 22:01:48 +0300

    web flask

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" Flask framework """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ returns 'Hello Hbnb' """
+    return "Hello HBNB!"
+
+
+@app.route("/HBNB", strict_slashes=False)
+def HBNB():
+    """ returns 'HBNB' """
+    return "NBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def text(text):
+    """ returns text given """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python", defaults={'text': 'is cool'})
+@app.route("/python/text", strict_slashes=False)
+def display(text):
+    """ display 'python' followed by the
+    value of the text
+    """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_n(n):
+    """ print n is a number """
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run()
